9b7454902c1964fa0625c493f8f63cbd3a9fc2dc
Delio Vicini <delio.vicini@gmail.com>
2020-11-25 12:32:09 +0100

    Some further cleanup and performance improvements

--- optimization.py
+++ optimization.py
@@ -1,11 +1,13 @@
-import scipy.stats as st
-import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
+
+import numpy as np
+
+import scipy
+import scipy as sp
+import scipy.stats as st
 
 import tqdm
 import tqdm.notebook
-import scipy
 
 
 class DatasetGenerator(object):
@@ -37,53 +39,51 @@ def compute_beta(N0, N1, f0, f1, epsilon):
 
 
 def objective_function(N, l, cities, selected_cities, pairwise_distances):
-    # Compute maximum area
     max_area = np.pi / 4 * np.max(np.outer(selected_cities, selected_cities) * pairwise_distances)
-    # Compute final loss
     f = np.sum(selected_cities * cities.v) - l * N * max_area
     return -f
 
 
-def acceptance_pb(selected_cities_i, selected_cities_j, beta, N, l, cities, pairwise_distances):
-    fi = objective_function(N, l, cities, selected_cities_i, pairwise_distances)
-    fj = objective_function(N, l, cities, selected_cities_j, pairwise_distances)
-    result = np.exp(-beta * (fj - fi))
-    return min(1, result)
-
-
-def step(N, cities, selected_cities_i, beta, l, pairwise_distances):
+def step(N, cities, selected_cities_i, current_loss_value, beta, l, pairwise_distances, mutation_strategy=0):
     k = np.random.randint(0, N)
-    if np.random.rand() < 0.5:  # Remove a city
-        if selected_cities_i[k] == 0:
-            return selected_cities_i  # same state then before, do nothing
-        else:
-            selected_cities_k = np.copy(selected_cities_i)
-            selected_cities_k[k] = 0  # city removed from set
-            a_ik = acceptance_pb(selected_cities_i, selected_cities_k, beta, N, l, cities, pairwise_distances)
-            return selected_cities_k if np.random.rand() < a_ik else selected_cities_i
-    else:  # Add a city
-        if selected_cities_i[k] == 1:  # do nothing, city already in set
-            return selected_cities_i
-        else:
-            selected_cities_k = np.copy(selected_cities_i)
-            selected_cities_k[k] = 1  # add city to set
-            a_ik = acceptance_pb(selected_cities_i, selected_cities_k, beta, N, l, cities, pairwise_distances)
-            return selected_cities_k if np.random.rand() < a_ik else selected_cities_i
+    remove_city = np.random.rand() < 0.5
+    if (mutation_strategy == 0) and ((remove_city and selected_cities_i[k] == 0) or (not remove_city and selected_cities_i[k] == 1)):
+        return (selected_cities_i, current_loss_value)  # do nothing
+    else:
+        selected_cities_k = np.copy(selected_cities_i)
+        selected_cities_k[k] = 1 - selected_cities_k[k]
+        new_loss_value = objective_function(N, l, cities, selected_cities_k, pairwise_distances)
+        a_ik = min(1, np.exp(-beta * (new_loss_value - current_loss_value)))
+        return (selected_cities_k, new_loss_value) if np.random.rand() < a_ik else (selected_cities_i, current_loss_value)
 
 
-def optimize(cities, l, beta=100, n_iter=20000):
+def optimize(cities, l, beta=100, n_iter=20000, mutation_strategy=0, initial_selection_probability=0.5):
+    """mutation_strategy = 0: Original mutation proposed by Heloise
+       mutation_strategy = 1: Simple strategy which just randomly tries to flip cities
+       initial_selection_probability: Probablity at which a city initially is selected (0.5: every city can be selected with 50% chance)"""
+
     N = cities.x.shape[0]
-    selected_cities = np.random.randint(2, size=N)
+    selected_cities = (np.random.rand(N) <= initial_selection_probability).astype(np.int32)
     pairwise_distances = scipy.spatial.distance.squareform(scipy.spatial.distance.pdist(cities.x, 'sqeuclidean'))
     fs = np.zeros(n_iter)
-    fs[0] = objective_function(N, l, cities, selected_cities, pairwise_distances)
+    current_loss_value = objective_function(N, l, cities, selected_cities, pairwise_distances)
     for m in tqdm.notebook.tqdm(range(n_iter)):
-        fs[m] = objective_function(N, l, cities, selected_cities, pairwise_distances)
-        selected_cities = step(N, cities, selected_cities, beta, l, pairwise_distances)
+        fs[m] = current_loss_value
+        selected_cities, current_loss_value = step(
+            N, cities, selected_cities, current_loss_value, beta, l, pairwise_distances,
+            mutation_strategy=mutation_strategy)
     return selected_cities, fs
 
 
 # Old code
+# Currently unused: We now use the objective function evaluation from the previous iteration to
+# improve performance
+# def acceptance_pb(selected_cities_i, selected_cities_j, beta, N, l, cities, pairwise_distances):
+#     fi = objective_function(N, l, cities, selected_cities_i, pairwise_distances)
+#     fj = objective_function(N, l, cities, selected_cities_j, pairwise_distances)
+#     result = np.exp(-beta * (fj - fi))
+#     return min(1, result)
+
 # def pbij(selected_cities_i, selected_cities_j, beta, N, l, citiesV):
 #     # not used in the algorithm, could be useful to plot and compare statistics
 #     if selected_cities_i == selected_cities_j:
