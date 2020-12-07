e891a216bd6930193848f048a25c8aad097fb158
ahmadajal <ajal.ahmad@gmail.com>
2020-12-07 15:53:20 +0000

    more efficient solution

--- optimal_solution.py
+++ optimal_solution.py
@@ -24,7 +24,7 @@ def bruteforce_sol(N, l, generator):
 
 
 def opt_solution(N, l, generator, verbose=False):
-    # better solution: O(N*3)
+    # better solution
     min_f = np.inf
     pairwise_distances = scipy.spatial.distance.squareform(
         scipy.spatial.distance.pdist(generator.x, 'sqeuclidean'))
@@ -35,14 +35,16 @@ def opt_solution(N, l, generator, verbose=False):
             max_dist = scipy.spatial.distance.euclidean(generator.x[first_city], 
                                                         generator.x[second_city])
             selected_cities[[first_city, second_city]] = 1
-            other_cities = np.delete(np.arange(N), [first_city, second_city])
-            for c in other_cities:
-                d_to_first = scipy.spatial.distance.euclidean(generator.x[first_city], 
-                                                        generator.x[c])
-                d_to_second = scipy.spatial.distance.euclidean(generator.x[second_city], 
-                                                        generator.x[c])
-                if (d_to_first <= max_dist) & (d_to_second <= max_dist):
-                    selected_cities[c] = 1
+            dist_to_fisrt = np.sqrt(((generator.x - generator.x[first_city])**2).sum(axis=1))
+            dist_to_second = np.sqrt(((generator.x - generator.x[second_city])**2).sum(axis=1))
+            # filter the cities whose distance to first is less than max_dist
+            cities_to_first = np.where(dist_to_fisrt <= max_dist)[0]
+            # filter the cities whose distance to second is less than max_dist
+            cities_to_second = np.where(dist_to_second <= max_dist)[0]
+            # selected cities is the intersection of the above sets
+            selected_cities_idx = np.intersect1d(cities_to_first, cities_to_second)
+            # add the new cities to the list
+            selected_cities[selected_cities_idx] = 1
             f = objective_function_simple(N, l, generator, selected_cities, 
                                       pairwise_distances=pairwise_distances)
             
