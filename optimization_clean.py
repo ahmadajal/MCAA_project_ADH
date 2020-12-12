8564375fcd1389ffd4d75188013db83db79087c3
Delio Vicini <delio.vicini@gmail.com>
2020-12-12 12:00:47 +0100

    parallelization, run MC on radius instead of radius squared, reduce mem consumption

--- optimization_clean.py
+++ optimization_clean.py
@@ -15,14 +15,15 @@ import copy
 
 
 def adding_convex_points(N, l, cities, state):
-    convex_hull = state['convex_hull']
+    selected_cities = state['selected']
+    selected_cities_pos = cities.x[selected_cities, :]
+    convex_hull = selected_cities_pos[ConvexHull(selected_cities_pos).vertices, :]
+    max_distance = np.max(scipy.spatial.distance.pdist(convex_hull, 'sqeuclidean'))
     hull_path = Path(convex_hull)
     is_in_hv = hull_path.contains_points(cities.x)
-    selected_cities = state['selected']
     # for some known bug in the function, if points is a border is_in_hv can return false
-    is_in_hv = (is_in_hv | (selected_cities == 1))*1
-    max_distance = state['max_dist']  # not changed by adding points in convex hull
-    loss = -np.sum(is_in_hv * cities.v) + l * N * max_distance * np.pi / 4
+    is_in_hv[selected_cities] = 1
+    loss = -np.sum(cities.v[is_in_hv == 1]) + l * N * max_distance * np.pi / 4
     return is_in_hv, loss
 
 
@@ -38,12 +39,12 @@ def maximum_dist(cities_pos):
 
 
 def objective_function(N, l, cities, selected_cities):
-    selected_cities_pos = cities.x[selected_cities == 1, :]
+    selected_cities_pos = cities.x[selected_cities, :]
     max_distance = maximum_dist(selected_cities_pos)
-    return -np.sum(selected_cities * cities.v) + l * N * max_distance * np.pi / 4
+    return -np.sum(cities.v[selected_cities]) + l * N * max_distance * np.pi / 4
 
 
-def step(N, cities, state, beta, l):
+def step(N, cities, state, beta, l, use_kd_tree=True):
     current_loss_value = state['loss_value']
     if np.random.rand() < 0.5:
         if np.random.rand() < 0.95:
@@ -55,7 +56,11 @@ def step(N, cities, state, beta, l):
         new_center = state['center']
         new_radius = np.maximum(0.01, state['radius'] + np.random.randn(1) * state['radius'] * 0.1)
 
-    selected_cities_k = (np.sum((cities.x - new_center) ** 2, axis=1) <= new_radius).astype(np.int32)
+    if use_kd_tree:
+        selected_cities_k = np.array(state['kdtree'].query_ball_point(new_center, float(new_radius))).astype(np.int32)
+    else:
+        selected_cities_k = np.where(np.sum((cities.x - new_center) ** 2, axis=1) <= float(new_radius) ** 2)[0]
+
     if selected_cities_k.shape[0] == 0:
         new_loss_value = np.inf
     else:
@@ -74,7 +79,7 @@ def step(N, cities, state, beta, l):
     return state
 
 
-def optimize(cities, l, beta=100, n_iter=20000, verbose=True):
+def optimize(cities, l, beta=100, n_iter=20000, verbose=True, use_kd_tree=True):
 
     if not callable(beta):
         def beta_fn(_):
@@ -85,13 +90,17 @@ def optimize(cities, l, beta=100, n_iter=20000, verbose=True):
     N = cities.x.shape[0]
 
     fs = np.zeros(n_iter)
-    all_selected_cities = []
 
     initial_center = np.random.rand(2)
-    initial_radius = np.random.rand(1) * 0.03 + 0.2
+    initial_radius = np.sqrt(np.random.rand(1) * 0.03 + 0.2)
 
     # Compute all the cities inside the circle
-    selected_cities = (np.sum((cities.x - initial_center) ** 2, axis=1) <= initial_radius).astype(np.int32)
+    if use_kd_tree:
+        kd_tree = scipy.spatial.cKDTree(cities.x)
+        selected_cities = kd_tree.query_ball_point(initial_center, float(initial_radius))
+    else:
+        kd_tree = None
+        selected_cities = np.where(np.sum((cities.x - initial_center) ** 2, axis=1) <= float(initial_radius) ** 2)[0]
 
     current_loss_value = objective_function(N, l, cities, selected_cities)
     it = tqdm.notebook.tqdm(range(n_iter)) if verbose else range(n_iter)
@@ -100,27 +109,42 @@ def optimize(cities, l, beta=100, n_iter=20000, verbose=True):
         'selected': selected_cities,
         'loss_value': current_loss_value,
         'center': initial_center,
-        'radius': initial_radius
+        'radius': initial_radius,
+        'kdtree': kd_tree
     }
 
+    all_selected_cities = []
     for m in it:
         fs[m] = state['loss_value']
-        state = step(N, cities, state, beta_fn(m), l)
+        state = step(N, cities, state, beta_fn(m), l, use_kd_tree=use_kd_tree)
+        if N <= 20000:
+            all_selected_cities.append(state['selected'])
+
+    # Select the lowest error iteration
+
+
+    if N <= 20000:
+        min_idx = np.argmin(fs)
+        state['selected'] = all_selected_cities[min_idx]
+        state['loss_value'] = fs[min_idx]
+
+        if state['selected'].shape[0] >= 3:
+            all_selected_cities_convex, fs_convex = adding_convex_points(N, l, cities, state)
+        else:
+            all_selected_cities_convex, fs_convex = state['selected'], state['loss_value']
+
+        if fs_convex > fs[-1]:
+            print("Convex ", fs_convex, " vs ", state['loss_value'])
+
+
+    else:
         all_selected_cities.append(state['selected'])
 
-    selected_cities_pos = cities.x[state['selected'] == 1, :]
-    if np.sum(state['selected']) >= 3:
-        state['convex_hull'] = selected_cities_pos[ConvexHull(selected_cities_pos).vertices, :]
-    state['max_dist'] = maximum_dist(selected_cities_pos)
+    for i in range(len(all_selected_cities)):
+        sel = np.zeros(N, np.int32)
+        sel[all_selected_cities[i]] = 1
+        all_selected_cities[i] = sel
 
-    if np.sum(state['selected']) >= 3:
-        all_selected_cities_convex, fs_convex = adding_convex_points(N, l, cities, state)
-    else:
-        all_selected_cities_convex, fs_convex = state['selected'], state['loss_value']
-
-    if fs_convex > fs[-1]:
-        print("Convex ", fs_convex, " vs ", fs[-1])
-
-    all_selected_cities[-1] = all_selected_cities_convex
-    fs[-1] = fs_convex
+    # all_selected_cities[-1] = all_selected_cities_convex
+    # fs[-1] = fs_convex
     return all_selected_cities, fs
