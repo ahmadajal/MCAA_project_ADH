73c30563e1c59b75aa2c7d79747c7724dd24c6f6
ahmadajal <ajal.ahmad@gmail.com>
2020-12-13 13:16:54 +0000

    optimization with clusters - ahmad

--- cluster_optimization.py
+++ cluster_optimization.py
@@ -14,8 +14,7 @@ from optimal_solution import *
 from sklearn.cluster import KMeans
 
 
-
-def optimize_with_clusters(g, l, selected_cities_init, betas=[5, 10, 20, 50, 100], num_clusters=[10, 100],
+def optimize_with_clusters(g, l, beta, num_clusters=[10, 100],
                            n_iter=20000, mutation_strategy=1, initial_selection_probability=0.0, 
                            precompute_pairwise_dist=False, verbose=True):
     c_n_iter = n_iter // (2*len(num_clusters))
@@ -30,24 +29,38 @@ def optimize_with_clusters(g, l, selected_cities_init, betas=[5, 10, 20, 50, 100
                 self.x = centers
                 self.v = population
         clusters = clusters()
+#         c_n_iter = n_iter//4
         print(c_n_iter)
         if i==0:
             c_selected_cities=(np.random.rand(len(centers)) <= initial_selection_probability).astype(np.int32)
         else:
             c_selected_cities=np.zeros(len(centers))
-            c_selected_cities[np.unique(kmeans.labels_[np.where(np.in1d(prev_labels, np.nonzero(c_cities_n_convex))==True)[0]])] = 1
-        c_cities_n, c_cities_n_convex, c_loss_values, c_loss_value_convex = optimize_with_initialize_betas(clusters, l, c_selected_cities, betas=betas, 
+            # pick only mega cities from the following set
+            init_set = np.unique(kmeans.labels_[np.where(np.in1d(prev_labels, np.nonzero(c_cities_n_convex))==True)[0]])
+            # number of cities to pick for initialization
+            if str(g)=="G2":
+                s = int(1 + np.sqrt(len(init_set))*(2-l)*len(init_set)/len(init_set))
+            else:
+                s = int(1 + np.sqrt(len(init_set))*(1-l)*len(init_set)/len(init_set))
+            c_selected_cities[np.random.choice(init_set, replace=False, size=s)] = 1
+        c_cities_n, c_cities_n_convex, c_loss_values, c_loss_value_convex = optimize(clusters, l, beta=beta, 
                                                   n_iter=c_n_iter, mutation_strategy=mutation_strategy, 
                                                   initial_selection_probability=initial_selection_probability,
-                                                  precompute_pairwise_dist=False, verbose=True)
+                                                  precompute_pairwise_dist=False, verbose=True, selected_cities=c_selected_cities)
         prev_labels = kmeans.labels_
     #### 
     # run on all cities
     full_n_iter = n_iter - len(num_clusters) * c_n_iter
     full_selected_cities=np.zeros(len(g.x))
-    full_selected_cities[np.where(np.in1d(kmeans.labels_, np.nonzero(c_cities_n_convex))==True)[0]] = 1
-    cities_n, cities_n_convex, loss_values, loss_value_convex = optimize_with_initialize_betas(g, l, full_selected_cities, betas=betas, 
+    init_set = np.where(np.in1d(kmeans.labels_, np.nonzero(c_cities_n_convex))==True)[0]
+    # number of cities to pick for initialization
+    if str(g)=="G2":
+        s = int(1 + np.sqrt(len(init_set))*(2-l)*len(init_set)/len(init_set))
+    else:
+        s = int(1 + np.sqrt(len(init_set))*(1-l)*len(init_set)/len(init_set))
+        full_selected_cities[np.random.choice(init_set, replace=False, size=s)] = 1
+    cities_n, cities_n_convex, loss_values, loss_value_convex = optimize(g, l, beta=beta, 
                                               n_iter=full_n_iter, mutation_strategy=mutation_strategy, 
                                               initial_selection_probability=initial_selection_probability,
-                                              precompute_pairwise_dist=False, verbose=True)
+                                              precompute_pairwise_dist=False, verbose=True, selected_cities=full_selected_cities)
     return cities_n, cities_n_convex, loss_values, loss_value_convex
