476b8d4272f7d1212fd63f8ff1d957f9de786884
ahmadajal <ajal.ahmad@gmail.com>
2020-12-17 11:36:22 +0000

    fix to IO

--- IO_read_write.py
+++ IO_read_write.py
@@ -5,17 +5,17 @@ def input_read(path_to_file):
     df = pd.read_csv(path_to_file)
     class G(object):
         def __init__(self):
-            self.x = np.array(df[["position x", "position y"]])
-            self.v = np.array(df["normalized population"])
+            self.x = np.array(df[["coordinates_x", "coordinates_y"]])
+            self.v = np.array(df["population"])
     g = G()
     return g
 
 def write_output(selected_cites, output_file_name):
-    d = {"city id": np.arange(1, len(selected_cites)+1),
-        "selected": selected_cites.astype(int)}
+    d = {"id": np.arange(1, len(selected_cites)+1),
+        "include": selected_cites.astype(int)}
     df = pd.DataFrame(d)
-    df.to_csv(output_file_name+".csv", header=False, index=False)
+    df.to_csv(output_file_name+".csv", index=False)
     
 def read_output(path_to_file):
-    df = pd.read_csv(path_to_file, header=None)
-    return np.array(df[1])
+    df = pd.read_csv(path_to_file)
+    return np.array(df["include"])
